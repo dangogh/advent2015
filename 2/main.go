a385c55eb3191e88ab0719b204a3e5b65f156b4d
Dan Kirkwood <daniel_kirkwood@cable.comcast.com>
2015-12-16 10:11:12 -0700

    finished part 2

--- 2/main.go
+++ 2/main.go
@@ -11,20 +11,73 @@ import (
 	"strings"
 )
 
-func dimensions(line string) ([]int, error) {
-	p := make([]int, 0)
+type Box []int
+
+func (b Box) Dimensions() []int {
+	return []int(b)
+}
+
+func (b Box) Perimeters() []int {
+	var s []int
+	for i, d0 := range b.Dimensions() {
+		for j, d1 := range b.Dimensions() {
+			if i != j {
+				s = append(s, 2*(d0+d1))
+			}
+		}
+	}
+	return s
+}
+
+func (b Box) SideAreas() []int {
+	var s []int
+	for i, d0 := range b.Dimensions() {
+		for j, d1 := range b.Dimensions() {
+			if i != j {
+				s = append(s, d0*d1)
+			}
+		}
+	}
+	return s
+}
+
+func (b Box) SurfaceArea() int {
+	s := b.SideAreas()
+	t := 0
+	for _, a := range s {
+		t += a
+	}
+	return t
+}
+
+func (b Box) WrappingArea() int {
+	s := b.SideAreas()
+	return b.SurfaceArea() + s[0]
+}
+
+func (b Box) Volume() int {
+	v := 1
+	for _, d := range b.Dimensions() {
+		v = v * d
+	}
+	return v
+}
+
+func NewBox(line string) (*Box, error) {
+	p := &Box{}
 	for _, s := range strings.Split(line, "x") {
 		v, err := strconv.Atoi(s)
 		if err != nil {
 			return nil, err
 		}
-		p = append(p, v)
+		*p = append(*p, v)
 	}
-	if len(p) != 3 {
-		return nil, errors.New(fmt.Sprintf("Expected 3 parts to %s but got %d", line, len(p)))
+	if len(*p) != 3 {
+		return nil, errors.New(fmt.Sprintf("Expected 3 dimensions, not %d on %s", len(*p), line))
 	}
 
-	sort.Ints(p)
+	// sort dimensions increasing order
+	sort.Ints(*p)
 	return p, nil
 }
 
@@ -39,16 +92,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	total := 0
+	wrappingTotal := 0
+	ribbonTotal := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		dim, err := dimensions(line)
+		b, err := NewBox(line)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		a := 3*dim[0]*dim[1] + 2*dim[0]*dim[2] + 2*dim[1]*dim[2]
-		total += a
+		wrappingTotal += b.WrappingArea()
+		p := b.Perimeters()
+		ribbonTotal += p[0] + b.Volume()
 	}
-	fmt.Printf("Total of %d sq ft\n", total)
+	fmt.Printf("Total wrapping paper area of %d sq ft\n", wrappingTotal)
+	fmt.Printf("Total ribbon length of %d ft\n", ribbonTotal)
 }
