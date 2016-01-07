b2b0eb576488807491c20a47a7f39ebf1d1d89c6
Dan Kirkwood <dangogh@gmail.com>
2016-01-06 22:20:00 -0700

    deadlock

--- 9/main.go
+++ 9/main.go
@@ -9,6 +9,22 @@ import (
 	"strings"
 )
 
+type cityList []string
+
+func (c cityList) Len() int {
+	return len(c)
+}
+
+func (c cityList) Less(i, j int) bool {
+	sl := []string(c)
+	return sl[i] < sl[j]
+}
+
+func (c cityList) Swap(i, j int) {
+	sl := []string(c)
+	sl[i], sl[j] = sl[j], sl[i]
+}
+
 func main() {
 	var reader *bufio.Reader
 	if len(os.Args) == 1 {
@@ -43,28 +59,57 @@ func main() {
 		city2city[c1][c2] = dist
 		city2city[c2][c1] = dist
 	}
-	cities := make([]string, len(city2city))
-	for c, _ := range city2city {
-	  cities = append(cities, c)
+
+	cities := make(cityList, len(city2city))
+	for c := range city2city {
+		cities = append(cities, c)
 	}
-	mathutil.PermuteFirst(cities)
-	for {
-	  go func() {
-		cityCopy := make([]string, len(cities))
-		copy(cityCopy, cities)
-		var there string
-		for _, here := range cityCopy {
-			if there == "" {
-			  there = here
-			  continue
+
+	// channel for permutations
+	permch := make(chan cityList)
+	mathutil.PermutationFirst(cities)
+
+	go func() {
+		c := make(cityList, len(cities))
+		copy(c, cities)
+		for {
+			permch <- c
+			if !mathutil.PermutationNext(c) {
+				break
 			}
-			// how far from there to here
-			dist += city2city[there][here]
 		}
-	  }
-	  if !mathutil.PermuteNext(cities) {
-	  	break
-	  }
+	}()
+
+	const MaxUint = ^uint(0)
+	mindist := MaxUint
+
+	ch := make(chan uint)
+	for p := range permch {
+		go func(p cityList) {
+			var dist uint
+			var there string
+			for _, here := range p {
+				if there == "" {
+					there = here
+					continue
+				}
+				// how far from there to here
+				dist += city2city[there][here]
+				if dist > mindist {
+					// give up without sending back value
+					return
+				}
+			}
+			// completed -- return distance calculated
+			ch <- dist
+		}(p)
 	}
-	fmt.Printf("city map: %v\n", city2city)
+
+	for dist := range ch {
+		if dist < mindist {
+			mindist = dist
+		}
+	}
+
+	fmt.Printf("Min distance is %d\n", mindist)
 }
