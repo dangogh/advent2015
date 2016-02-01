1f976133297c85f3c841dd853fe4902939e12e03
Dan Kirkwood <daniel_kirkwood@cable.comcast.com>
2016-02-01 12:51:57 -0700

    part1 complete

--- 12/main.go
+++ 12/main.go
@@ -2,56 +2,70 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"strconv"
-	"strings"
 	"unicode"
 )
 
-func isNum(c byte) bool {
-	return (c == '-') || (unicode.IsDigit(rune(c)))
+func isNum(c rune) bool {
+	return unicode.IsDigit(c)
 }
 
-func addInts(a string) int {
-	s := bufio.NewScanner(strings.NewReader(a))
-	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		fmt.Printf("string is %s\n", string(data))
-		var i int
-		if len(data) == 0 {
+func splitNumbers(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if len(data) == 0 {
+		return 0, data, nil
+	}
+
+	var i int
+	if rune(data[0]) == '-' || isNum(rune(data[0])) {
+		i = bytes.IndexFunc(data[1:], func(b rune) bool { return !isNum(b) })
+	} else {
+		i = bytes.IndexFunc(data[1:], func(b rune) bool { return isNum(b) || b == '-' })
+	}
+	if i != -1 {
+		return i + 1, data[0 : i+1], nil
+	} else {
+		if atEOF {
+			return len(data), data, nil
+		} else {
 			return 0, nil, nil
 		}
-		c := data[0]
-		isnum := isNum(c)
-		for i, c = range data {
-			if isNum(c) != isnum {
-				fmt.Printf(" token %s is num? %v\n", string(data[:i]), isnum)
-				return i, data[:i], nil
-			}
-		}
-		fmt.Printf(" token %s is num? %v\n", string(data), isnum)
-		return len(data), data, nil
 	}
-	s.Split(split)
+}
+
+func addInts(r io.Reader) int {
+	s := bufio.NewScanner(r)
+
+	s.Split(splitNumbers)
 	var sum int
 	for s.Scan() {
 		tok := s.Bytes()
 		if len(tok) == 0 {
-			//break
+			break
 		}
 		num, err := strconv.Atoi(string(tok))
 		if err == nil {
-			fmt.Println("Found number ", num)
+			fmt.Println(num)
 			sum += num
 		}
 	}
-	fmt.Printf("  last token %s\n", string(s.Bytes()))
 	return sum
 }
 
 func main() {
-	for _, a := range os.Args[1:] {
-		s := addInts(a)
-		fmt.Printf("%d\t%s\n", s, a)
+	f := os.Stdin
+	if len(os.Args) > 1 {
+		var err error
+		f, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+	reader := bufio.NewReader(f)
+	s := addInts(reader)
+	fmt.Printf("%d\n", s)
 }
