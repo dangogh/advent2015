3cf38a03d40658261044b9a72aad72529a39e9db
Dan Kirkwood <daniel_kirkwood@cable.comcast.com>
2016-03-09 21:52:56 -0700

    day 20 part2

--- 20/main.go
+++ 20/main.go
@@ -7,10 +7,15 @@ var houses []int
 func main() {
 
 	n := 29000000
-	house := make([]int, n/10)
+	house := make([]int, 1, n/10)
 	for i := 1; i < n/10; i++ {
-		for j := i; j < n/10; j += i {
-			house[j] += i * 10
+		var count int
+		for j := i; count < 50; j += i {
+			for len(house) <= j {
+				house = append(house, 0)
+			}
+			house[j] += i * 11
+			count++
 		}
 		if house[i] > n {
 			fmt.Println(i, " has ", house[i])
